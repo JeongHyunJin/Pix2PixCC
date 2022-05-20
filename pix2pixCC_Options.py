06fd4664551b0509dc736949ba951b822621043f
Hyun-Jin Jeong <jeong_hj@khu.ac.kr>
2022-05-20 13:41:15 +0900

    Update pix2pixCC_Options.py

--- pix2pixCC_Options.py
+++ pix2pixCC_Options.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Options for the pix2pixCC model
-
+Options of the Pix2PixHD Model
+@author: Hyun-Jin Jeong (https://jeonghyunjin.com, jeong_hj@khu.ac.kr)
+Reference:
+1) https://github.com/JeongHyunJin/Pix2PixCC
+2) https://arxiv.org/pdf/2204.12068.pdf
 """
 
 #==============================================================================
